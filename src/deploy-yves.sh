9a8f6512494d99f079f1bbf7a8f942a1797c2938
John Kheir <kheirj@codescalers.com>
2016-06-15 15:34:08 +0200

    Making scripts generic

--- src/deploy-yves.sh
+++ src/deploy-yves.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 # First argument of this script should be the password of the cloudscalers user on the machine hosting the docker WHMCS container
+# Second argument is of format username@cloudspace_ip
+# Third argument is the SSH port of the virtual machine
+# Fourth argument is the name of the Docker container WHMCS is running on
 set -euf
 rootpassword=$1
+remote_host=$2
+remote_ssh_port=$3
+container_name=$4
 tar -cvzf cockpit.tgz whmcs
-scp -P 2222 cockpit.tgz cloudscalers@85.255.197.104:/tmp/
-scp -P 2222 docker-deploy-yves.sh cloudscalers@85.255.197.104:/tmp/docker-deploy.sh
-ssh -p 2222 -t cloudscalers@85.255.197.104 "echo $rootpassword | sudo -S -i bash /tmp/docker-deploy.sh"
+scp -P ${remote_ssh_port} cockpit.tgz ${remote_host}:/tmp/
+scp -P ${remote_ssh_port} docker-deploy-yves.sh ${remote_host}:/tmp/docker-deploy.sh
+ssh -p ${remote_ssh_port} -t ${remote_host} "echo $rootpassword | sudo -S -i bash /tmp/docker-deploy.sh ${container_name}"
 rm -f cockpit.tgz
--- src/docker-deploy-yves.sh
+++ src/docker-deploy-yves.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 set -euf
+container_name=$1
 cd /tmp
 rm -rf whmcs html
 tar -xvf cockpit.tgz
 mv whmcs html
 mv html/templates/itsyouonline html/templates/fusion
-docker cp html whmcs:/var/www/
-rm -rf html cockpit.tgz docker-deploy-yves.sh
+docker cp html ${container_name}:/var/www/
+rm -rf html cockpit.tgz docker-deploy.sh
